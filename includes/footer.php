f58eccaee5bff7142d51c4197c220f6396c9800a
Matt <matty_noye@live.com>
2018-02-19 02:07:43 +0000

    Footer file created & content copied over
    Footer file has been created and i have also copied over the footer content from the index page.  
    
    This will make it easier to manage the footer sections throughout the whole theme

---
+++ includes/footer.php
@@ -0,0 +1,36 @@
+        </div><!-- closes site-wrapper -->
+        <footer>
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-6">
+                        <div id="copyright">
+                            <p>
+                                Copyright &copy; <?php echo date('Y'); ?> | All rights reserved.
+                            </p> 
+                        </div>
+                    </div>
+                    <div class="col-lg-6">
+                        <div id="des-by">
+                            <p>
+                                Designed by <a href="https://www.mwn-design.co.uk" target="_blank">MWN Design</a>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </footer>
+        <div id="search">
+            <button type="button" class="close"> X </button>
+            <form>
+                <input type="search" value="" placeholder="Search for a product">
+                <button type="submit" class="btn btn-ccw-lg">Search</button>
+            </form>
+        </div>
+        <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+        <script src="assets/js/jquery-1.12.0.min.js"></script>
+        <!-- Include all compiled plugins (below), or include individual files as needed -->
+        <script src="assets/js/bootstrap.js"></script>
+        <script src="assets/js/script.js"></script>
+    </body>
+</html>
