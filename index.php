2de4bde88beedc2c73b4754d4faa2d83fb2eb8b2
Matt <matty_noye@live.com>
2018-06-29 03:49:26 +0100

    Started work on index page
    I have now started work on the index page.  I have added the top section of the index page, this includes a welcome message to the left.  On the right i will be adding in a image slider.
    
    I have also added in a banner beneath the top section.

--- index.php
+++ index.php
@@ -1,3 +1,73 @@
 <?php include_once'includes/header.php'; ?>
 
+<section id="in-top">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-6">
+				<h1>
+					Welcome  to CCTV World
+				</h1>
+				<p>
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse dictum felis ante, et interdum lectus egestas non. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Integer fringilla sit amet ante feugiat sollicitudin.
+				</p>
+			</div><!-- closes col -->
+			<div class="col-md-6">
+
+			</div>
+		</div>
+	</div>
+</section><!-- closes in-top sect -->
+
+<section id="in-top-banner">
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-md-3">
+				<div class="block block-icon-left">
+					<div class="icon">
+						<i class="fa fa-envelope fa-5x"></i>
+					</div>
+					<div class="icon-content">
+						<h4>Free delivery</h4>
+						<h5>When you spend £100+</h5>
+					</div>
+				</div>
+			</div><!-- closes col -->
+			<div class="col-md-3">
+				<div class="block block-icon-left">
+					<div class="icon">
+						<i class="fa fa-phone fa-5x"></i>
+					</div>
+					<div class="icon-content">
+						<h4>Give us a ring</h4>
+						<h5>0800 123 4561</h5>
+					</div>
+				</div>
+			</div><!-- closes col -->
+			<div class="col-md-3">
+				<div class="block block-icon-left">
+					<div class="icon">
+						<i class="fa fa-envelope-open-o fa-5x"></i>
+					</div>
+					<div class="icon-content">
+						<h4>Email the team</h4>
+						<h5>team@cctvworld.uk</h5>
+					</div>
+				</div>
+			</div><!-- closes col -->
+			<div class="col-md-3">
+				<div class="block block-icon-left">
+					<div class="icon">
+						<i class="fa fa-lock fa-5x"></i>
+					</div>
+					<div class="icon-content">
+						<h4>Secure shopping</h4>
+						<h5>100% secure payments</h5>
+					</div>
+				</div>
+			</div><!-- closes col -->
+		</div>
+	</div>
+</section><!-- closes in-top-banner -->
+
+
 <?php include_once'includes/footer.php'; ?>
